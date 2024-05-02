84a85f20eb2326fe59ce3bc2a8e0f142a6180380
Steffen M. Noe <48022255+stenoe@users.noreply.github.com>
2024-05-02 11:16:57 +0000

    initial upload

--- Docker/models/models.go
+++ Docker/models/models.go
@@ -1,1 +1,9 @@
+package models
 
+import "gorm.io/gorm"
+
+type Measurement struct {
+	gorm.Model
+	Quantity string `json:"quantity" gorm:"text;not null;default:null"`
+	Value    string `json:"value" gorm:"text;not null;default:null"`
+}

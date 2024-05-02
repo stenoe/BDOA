6eb0b1f8221baf24bddc1b80785a712105bea7cb
Steffen M. Noe <48022255+stenoe@users.noreply.github.com>
2024-05-02 11:14:37 +0000

    initial upload

--- Docker/server3/database/database.go
+++ Docker/server3/database/database.go
@@ -1,1 +1,49 @@
+package database
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/stenoe/server3/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=db password=%s user=%s dbname=%s port=5432 sslmode=disable TimeZone=Europe/Tallinn",
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+	)
+
+	//log.Println(dsn)
+
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{Logger: logger.Default.LogMode(logger.Info)},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(1)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Measurement{})
+
+	DB = Dbinstance{
+		Db: db,
+	}
+}
